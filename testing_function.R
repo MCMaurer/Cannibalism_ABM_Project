2fa4d4eb85f52381bba6d92dd203e46fcc6a1c4b
MCMaurer <mjculshawmaurer@ucdavis.edu>
2016-11-07 14:03:31 -0800

    got the plots to work

--- testing_function.R
+++ testing_function.R
@@ -1,30 +1,51 @@
 setwd("/Users/MJ")
-data <- CannNetLogoToR("Github/Cannibalism_ABM_Project/Cannibalism_sensitivity_infected-death-modifier-spreadsheet.csv", 
+data <- CannNetLogoToR("Documents/Cannibalism_ABM_Project/Cannibalism_sensitivity_infected-death-modifier-spreadsheet.csv", 
                        c(1, 3:27), "Inf_Death_Modifier")
-
-View(data[1:50,])
 
 
 data2 <- CannNetLogoToR("Github/Cannibalism_ABM_Project/Cannibalism_sensitivity_inf-fecund-modifier-spreadsheet.csv", 
                         c(1:13, 15:27), "Inf_Fecund_modifier")
 
 
+write.csv(data, "Documents/Cannibalism_ABM_Project/InfDeathOutput.csv")
 
-View(data[1:50,])
+
+View(data %>% filter(Inf_Death_Modifier == -27))
 
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(wesanderson)
 
 
 
 data_small <- data %>%
   filter(grepl("[0]$", Time))
 
-data_small %>% 
-  group_by(Run, Time, type, Inf_Death_Modifier) %>% 
-  ggplot(aes(x=Time, y=Count, group=Run))+
-  geom_line(aes(colour=Inf_Death_Modifier))
+
+datasmall <- data_small[1:838838,]
+
+datasmall <- datasmall %>% 
+  filter(grepl("[0]$", Run))
+
+View(datasmall)
+
+View(data_small)
+
+datasmall$Inf_Death_Modifier <- as.numeric(datasmall$Inf_Death_Modifier)
+datasmall$Time <- as.numeric(datasmall$Time)
+datasmall$Count <- as.numeric(datasmall$Count)
+datasmall$type <- as.factor(datasmall$type)
+
+datasmall %>% 
+  #filter(Time <= 3000) %>% 
+  group_by(Run, type, Inf_Death_Modifier) %>% 
+  ggplot(aes(x=Time, y=Count, group=interaction(Run, type, Inf_Death_Modifier), alpha=0.2, colour=Inf_Death_Modifier, linetype=type))+
+  geom_line()+
+  scale_x_continuous(breaks = seq(0, 10000, 1000))+
+  scale_y_continuous(breaks = seq(0, 600, 50))+
+  scale_color_manual(values = wes_palette("Zissou", 21, type = "continuous"))+
+  theme_bw()
 
   
 View(head(data_small))
